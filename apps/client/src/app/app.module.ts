12e06caca43b12c9f42a0d987c630c14a94d3c44
Carson Wood <carsnwd@gmail.com>
2020-12-24 00:23:04 -0500

    🎉 init app with nx, angular, nest, and primeng

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
